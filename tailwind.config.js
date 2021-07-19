fcb9425b75bc06874069e07a23fb299429837fd3
Faizan <f24aalam@gmail.com>
2021-07-19 11:17:55 +0530

    Added Time and Date Component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '10xl': '14rem'
+      },
+    },
   },
   variants: {
     extend: {},
