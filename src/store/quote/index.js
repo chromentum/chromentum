561486a4d6e04330925037edcc0c1ad0ca597d91
Faizan <f24aalam@gmail.com>
2021-08-05 17:07:08 +0530

    Formatted the code

--- src/store/quote/index.js
+++ src/store/quote/index.js
@@ -5,39 +5,39 @@ import axios from "axios";
 Vue.use(Vuex);
 
 export default {
-    state: {
-        quote: {},
-        error: "",
-        status: "",
+  state: {
+    quote: {},
+    error: "",
+    status: "",
+  },
+  getters: {
+    quote: (state) => state.quote,
+    error: (state) => state.error,
+    status: (state) => state.status,
+  },
+  mutations: {
+    set_quote(state, quote) {
+      state.quote = quote;
+      state.status = "success";
     },
-    getters: {
-        quote: (state) => state.quote,
-        error: (state) => state.error,
-        status: (state) => state.status,
+    set_error(state, error) {
+      state.error = error;
+      state.status = "failure";
     },
-    mutations: {
-        set_quote(state, quote) {
-          state.quote = quote;
-          state.status = "success";
-        },
-        set_error(state, error) {
-          state.error = error;
-          state.status = "failure";
-        },
+  },
+  actions: {
+    fetchQuote({ commit, state }) {
+      state.status = "loading";
+      axios
+        .get("/quote")
+        .then((res) => {
+          commit("set_quote", res.data);
+        })
+        .catch((err) => {
+          commit("set_error", err.response.data);
+        });
     },
-    actions: {
-        fetchQuote({ commit, state }) {
-          state.status = "loading";
-          axios
-            .get("/quote")
-            .then((res) => {
-              commit("set_quote", res.data);
-            })
-            .catch((err) => {
-              commit("set_error", err.response.data);
-            });
-        },
-    },
-    modules: {},
-    namespaced: true,
-}
+  },
+  modules: {},
+  namespaced: true,
+};
